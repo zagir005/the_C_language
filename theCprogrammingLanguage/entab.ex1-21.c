9d4503ee23ede1be798d7deabdb94334baaacbec
gadzimirzoevz05@gmail.com <gadzimirzoevz05@gmail.com>
2023-10-19 16:56:14 +0300

    just refactor

--- theCprogrammingLanguage/entab.ex1-21.c
+++ theCprogrammingLanguage/entab.ex1-21.c
@@ -4,6 +4,13 @@
 #define IN 1
 #define OUT 0
 
+/*
+Write a program entab that replaces strings of blanks by the minimum 
+number of tabs and blanks to achieve the same spacing. Use the same tab stops as for detab. 
+When either a tab or a single blank would suffice to reach a tab stop, which should be given 
+preference?
+*/
+
 void entab_replace(char s[], char new_s[]);
 
 int main()
